1b3a3b14ea7b1658d42bccd98d10a5978d279809
arjakroy <arjakroy2411@gmail.com>
2026-03-07 11:51:33 +0530

    gen-k

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -2,6 +2,11 @@ import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/google-genai';
 
 export const ai = genkit({
-  plugins: [googleAI()],
+  plugins: [googleAI(
+    {
+      apiKey: "AIzaSyDwY7HO7bdOYuyFnMfbMg2sDblSYhgkTr0"
+    }
+  )],
   model: 'googleai/gemini-2.5-flash',
+  
 });
