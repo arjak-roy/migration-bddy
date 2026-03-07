3567000c5fb2a7b36edaac8c4b81111c8ee366c7
arjakroy <arjakroy2411@gmail.com>
2026-03-07 11:58:37 +0530

    security

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -4,7 +4,7 @@ import {googleAI} from '@genkit-ai/google-genai';
 export const ai = genkit({
   plugins: [googleAI(
     {
-      apiKey: "AIzaSyDwY7HO7bdOYuyFnMfbMg2sDblSYhgkTr0"
+      apiKey: process.env.GOOGLE_API_KEY,
     }
   )],
   model: 'googleai/gemini-2.5-flash',
